3ea4be9ce93b3b13a259c413d15e7b2f4f6cfd27
root <root@ns3050173.ip-151-80-26.eu>
2019-03-20 17:26:28 +0100

    First commit

---
+++ ddos_core.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+/sbin/iptables -F ;
+/sbin/iptables -A INPUT -s $IPADDR -p tcp -j ACCEPT ;
+/sbin/iptables -A INPUT -m set --match-set YNDX src -p tcp -j ACCEPT ;
+/sbin/iptables -A INPUT -m set --match-set YANDEX src -p tcp -j ACCEPT ;
+/sbin/iptables -A INPUT -m set --match-set GOOGLE src -p tcp -j ACCEPT ;
+/sbin/iptables -A INPUT -m set --match-set MICROSOFT src -p tcp -j ACCEPT ;
+/sbin/iptables -A INPUT -m set --match-set OPERA src -p tcp -j ACCEPT ;
+/sbin/iptables -A INPUT -m set --match-set OURADD src -p tcp -j ACCEPT ;
+/sbin/iptables -A INPUT -p tcp --dport 80 -j REJECT --reject-with icmp-port-unreachable ;
+/sbin/iptables -A INPUT -p tcp --dport 443 -j REJECT --reject-with icmp-port-unreachable ;
